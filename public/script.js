d827ddf08477e222ae7669f86d079e0ca7ce17e4
Camilla Thorsen <cthorsen@skypilot.dev>
2021-01-07 13:42:14 -0800

    feat: When guess input is checked with btn, give appropriate feedback

--- public/script.js
+++ public/script.js
@@ -5,9 +5,15 @@ document.querySelector('.secret-number').textContent = secretNumber;
 
 document.querySelector('.btn__check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess-input').value);
-
-    // If there is no guess, give feedback that a number must be inputted
     if (!guess) {
         document.querySelector('.message').textContent = 'Please type in a guess';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct number!';
+    } else if (guess > secretNumber && guess <= 20) {
+        document.querySelector('.message').textContent = 'Too high!';
+    } else if (guess < secretNumber && guess >= 1) {
+        document.querySelector('.message').textContent = 'Too low!';
+    } else {
+        document.querySelector('.message').textContent = 'Number must be between 1 and 20';
     }
 });
