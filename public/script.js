8e5174956c3c8e8caf7feb6855c108d99eb3ce16
Camilla Thorsen <cthorsen@skypilot.dev>
2021-01-07 16:15:17 -0800

    feat: Add feedback condition if inputted value is a decimal

--- public/script.js
+++ public/script.js
@@ -3,17 +3,19 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.secret-number').textContent = secretNumber;
 
-document.querySelector('.btn__check').addEventListener('click', function() {
-    const guess = Number(document.querySelector('.guess-input').value);
-    if (!guess) {
-        document.querySelector('.message').textContent = 'Please type in a guess';
-    } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Correct number!';
-    } else if (guess > secretNumber && guess <= 20) {
-        document.querySelector('.message').textContent = 'Too high!';
-    } else if (guess < secretNumber && guess >= 1) {
-        document.querySelector('.message').textContent = 'Too low!';
-    } else {
-        document.querySelector('.message').textContent = 'Number must be between 1 and 20';
-    }
+document.querySelector('.btn__check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess-input').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Please type in a guess';
+  } else if (guess !== Math.trunc(guess)) {
+    document.querySelector('.message').textContent = 'Must be a whole number (no decimals)';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+  } else if (guess > secretNumber && guess <= 20) {
+    document.querySelector('.message').textContent = 'Too high!';
+  } else if (guess < secretNumber && guess >= 1) {
+    document.querySelector('.message').textContent = 'Too low!';
+  } else {
+    document.querySelector('.message').textContent = 'Number must be between 1 and 20';
+  }
 });
